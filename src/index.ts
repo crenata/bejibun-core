5299b8f9535e4f9202fd91462e0b14ca9f0745fb
Hafiizh Ghulam <hafiizh.ghulam@gmail.com>
2025-10-12 21:53:58 +0700

    fix: export class based

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export * from "@/utils/Enum";
-export * from "@/utils/Str";
+import Enum from "@/utils/Enum";
+import Str from "@/utils/Str";
+import * as Utils from "@/utils/utils";
+
+export {Enum, Str};
+
 export * from "@/utils/utils";
+
+export default {
+    Enum,
+    Str,
+    ...Utils,
+};
